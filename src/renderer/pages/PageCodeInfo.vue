199c70d35df70d1b79603e1c491ddba3a39f5386
Алексей Тряпко <aatryapko@mail.ru>
2019-01-10 22:01:04 +0200

    Fix styles for btns

--- src/renderer/pages/PageCodeInfo.vue
+++ src/renderer/pages/PageCodeInfo.vue
@@ -1,15 +1,19 @@
 <template>
-<div>
+<div class="main">
+  <div class="footer">
+    <div class="top-btn">
+      <md-button class="md-raised md-primary" @click="loadEncodeData">Пройти тест на кодування</md-button>
+      <md-button class="md-raised md-primary" @click="showDialog = true">Пройти тест декодування</md-button>
+    </div>
+    <md-button class="md-raised md-primary bottom-btn" href="/">
+      <p>На головну</p>
+    </md-button>
+  </div>
   <div class="theory" v-html="template">
 
   </div>
 
-  <div v-html="view"></div>
-  <div class="footer">
-    <md-button class="md-raised md-primary" @click="loadEncodeData">Пройти тест на кодування</md-button>
-    <md-button class="md-raised md-primary" @click="showDialog = true">Пройти тест декодування</md-button>
-    <md-button class="md-big md-raised md-primary" href="/"><p>На головну</p></md-button>
-  </div>
+
 </div>
 </template>
 
@@ -27,7 +31,6 @@ export default {
     return {
       showDialog: false,
       template: '',
-      view: ''
     }
   },
   created() {
@@ -47,37 +50,52 @@ export default {
   methods: {
     loadEncodeData() {
       axios.post('http://127.0.0.1:9090/encodedata', {
-      module_name: this.id
-    })
-      .then(response => {
-      this.view = response.data.view;
-      console.log(response);
-    })
-    .catch(error => {
-      // handle error
-      console.log(error);
-    });
-    this.showDialog = true
+          module_name: this.id
+        })
+        .then(response => {
+          this.view = response.data.view;
+          console.log(response);
+        })
+        .catch(error => {
+          // handle error
+          console.log(error);
+        });
+      this.showDialog = true
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.theory{
-  
+.main {
+    display: flex;
+    flex-direction: row;
+    position: relative;
 }
-.matrix-bottom-text-area{
-  width: 500px;
+.top-btn{
+  position: sticky;
+  width: 250px;
+  top: 0;
 }
-.md-dialog-actions{
-  display: flex;
-  justify-content: space-around;
+.bottom-btn{
+  position: fixed;
+  bottom: 0;
+}
+.theory {
+    padding: 10px;
+    width: 90%;
+}
+.matrix-bottom-text-area {
+    width: 500px;
+}
+.md-dialog-actions {
+    display: flex;
+    justify-content: space-around;
 }
 .md-dialog {
     width: 500px;
     max-width: 768px;
-  }
+}
 .header {
     text-align: center;
     font-size: 23px;
@@ -86,11 +104,14 @@ export default {
     text-align: center;
 }
 .footer {
-  text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
 }
-.md-button {  
-  border: 1px solid black;
-  color: black;
+.md-button {
+    border: 1px solid black;
+    color: black;
 }
 .md-content {
     width: 200px;
